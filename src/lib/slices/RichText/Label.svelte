150d347f7dd5d955cd5ae7d84009113d431d45ac
Jatesh Parikh <jateshparikh97@gmail.com>
2024-08-16 09:51:39 +0530

    project setup

---
+++ src/lib/slices/RichText/Label.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { RTLabelNode } from '@prismicio/client';
+
+	export let node: RTLabelNode;
+</script>
+
+{#if node.data.label === 'codespan'}
+	<code><slot /></code>
+{:else}
+	<span class={node.data.label}><slot /></span>
+{/if}
---
+++ src/lib/slices/RichText/index.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { Content } from '@prismicio/client';
+	import { PrismicRichText } from '@prismicio/svelte';
+	import Label from './Label.svelte';
+
+	export let slice: Content.RichTextSlice;
+</script>
+
+<section class="">
+	<PrismicRichText
+		field={slice.primary.content}
+		components={{
+			label: Label
+		}}
+	/>
+</section>
